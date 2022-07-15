7abe18b6846c60436f29f11cec68b848cfa5a6a0
陳宇震 <tp6m35p4@gmail.com>
2022-07-15 12:49:17 +0800

    change to hash mode

--- src/route/index.js
+++ src/route/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../components/Home.vue';
 const routes = [
     {
