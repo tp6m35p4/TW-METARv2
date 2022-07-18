475f76fa964dc6bccfd0207ae51bf80a65e601b0
Yu-Cheng Chen <tp6m35p4@gmail.com>
2022-07-18 18:27:12 +0800

    add NOTAM decode

--- src/components/NotamAccordion.vue
+++ src/components/NotamAccordion.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="accordion" id="accordionPanelsStayOpenExample">
+  <div class="accordion">
     <div class="accordion-item" v-for="(i, k) in notams" :key="k">
       <h2 class="accordion-header" :id="`panelsStayOpen-heading-${k}`">
         <button
-          class="accordion-button collapsed d-flex gap-1"
+          class="accordion-button collapsed d-flex flex-wrap gap-1"
           type="button"
           data-bs-toggle="collapse"
           :data-bs-target="`#panelsStayOpen-collapse-${k}`"
@@ -20,6 +20,16 @@
             class="badge"
             :class="notamCode['validity'][i['validity']][1]"
             >{{ notamCode["validity"][i["validity"]][0] }}</span
+          >
+          <span
+            class="badge bg-secondary d-flex flex-wrap"
+            v-if="notamCode['code23'][i['parsed']['code23']]"
+            >{{ notamCode["code23"][i["parsed"]["code23"]][0] }}</span
+          >
+          <span
+            class="badge bg-secondary d-flex flex-wrap"
+            v-if="notamCode['code45'][i['parsed']['code45']]"
+            >{{ notamCode["code45"][i["parsed"]["code45"]][0] }}</span
           >
         </button>
       </h2>
--- src/helpers/notamParser.js
+++ src/helpers/notamParser.js
@@ -1,13 +1,16 @@
 // const typeRegex = /^A\d{4}\/\d{2} NOTAM([NRCS])\s{1,}Q/;
 const typeRegex = /(A\d*\/\d*)\sNOTAM([NRCS])\s{1,2}(A\d*\/\d*)*\s*Q\)/;
-const qualiferFieldsRegex = /Q\)\s(\w{4})\/(\w{5})\/([IV]{1,2})\/([NBOM]{1,4})\/([AEW]{1,3})\/(\d{3})\/(\d{3})\/(\w*)\s/;
+const qualiferFieldsRegex =
+  /Q\)\s(\w{4})\/(\w{5})\/([IV]{1,2})\/([NBOM]{1,4})\/([AEW]{1,3})\/(\d{3})\/(\d{3})\/(\w*)\s/;
 const ARegex = /A\)/;
 const BRegex = /B\)/;
 export function parse(raw) {
-    let type = raw.match(typeRegex).slice(1);
-    let q = raw.match(qualiferFieldsRegex).slice(1)
-    return {
-        type: type,
-        qulifer: q
-    };
+  let type = raw.match(typeRegex).slice(1);
+  let q = raw.match(qualiferFieldsRegex).slice(1);
+  return {
+    type: type,
+    qulifer: q,
+    code23: q[1].slice(1, 3),
+    code45: q[1].slice(3, 5),
+  };
 }
