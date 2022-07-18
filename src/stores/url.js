68c21e51c2825778c9fd5524096e4e20d68c9633
Yu-Cheng Chen <tp6m35p4@gmail.com>
2022-07-18 18:27:43 +0800

    deploy

--- src/stores/url.js
+++ src/stores/url.js
@@ -1,3 +1,4 @@
 export const windyNotamUrl = "https://node.windy.com/airports/notams";
-// export const metarUrl = "https://ptx.transportdata.tw/MOTC/v2/Air/METAR/Airport?$top=30&$format=JSON";
-export const metarUrl = "data.json";
+export const metarUrl =
+  "https://ptx.transportdata.tw/MOTC/v2/Air/METAR/Airport?$top=30&$format=JSON";
+// export const metarUrl = "data.json";
