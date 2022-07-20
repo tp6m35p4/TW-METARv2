885fc56e6aa032f2a05db92c8daf6bad201519bc
Yu-Cheng Chen <tp6m35p4@gmail.com>
2022-07-20 21:39:56 +0800

    add home

---
+++ src/components/Airport.vue
@@ -0,0 +1,101 @@
+<template>
+  <MetarCard :metar="metar" />
+  <div class="card m-4">
+    <div class="card-header d-flex justify-content-between">
+      <ul class="nav nav-tabs card-header-tabs">
+        <NotamNavItem
+          v-for="(i, k) in notamCode.validity"
+          :text="i[0]"
+          :type="k"
+          :key="k"
+        />
+      </ul>
+      <button @click="loadNotam" class="btn btn-secondary btn-sm">Load</button>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title">
+        NOTAMs of {{ id }}
+        <!-- {{ airport }} -->
+      </h5>
+      <NotamAccordion :notams="notam" />
+
+      <!-- <a href="#" class="card-link">Card link</a>
+      <a href="#" class="card-link">Another link</a> -->
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "@vue/reactivity";
+import { useRoute } from "vue-router";
+import { computed, watch, onMounted } from "vue";
+import NotamNavItem from "./NotamNavItem.vue";
+import { useNotam } from "../stores/selected";
+import { parse } from "../helpers/notamParser";
+import NotamAccordion from "./NotamAccordion.vue";
+import { notamCode } from "../stores/notamCode";
+import { windyNotamUrl } from "../stores/url";
+import MetarCard from "./MetarCard.vue";
+import ClipboardJS from "clipboard";
+const props = defineProps(["id", "airports"]);
+// const route = useRoute();
+const selectedNotamType = 0;
+const metar = computed(() => {
+  return props.airports.data[props.id];
+});
+const notams = reactive({
+  data: {},
+});
+
+const notamStore = useNotam();
+
+const notam = computed(() => {
+  if (notams.data[props.id] != undefined) {
+    if (notamStore.selectdState == 2) {
+      return notams.data[props.id];
+    } else {
+      return notams.data[props.id].filter((e) => {
+        return e["validity"] == notamStore.selectdState;
+      });
+    }
+  }
+  return [];
+});
+
+function loadNotam() {
+  fetch(`${windyNotamUrl}/${props.id}`)
+    // fetch("notam_RCTP.json")
+    .then((res) => {
+      if (res.ok) {
+        return res.json();
+      }
+      throw new Error("Get data error");
+    })
+    .then((data) => {
+      notams.data[props.id] = [];
+      data.forEach((e) => {
+        let fromTo = e["fromTo"].split("-");
+        let from = fromTo[0];
+        let to = fromTo[1];
+        let n = parse(e["raw"]);
+        notams.data[props.id].push({
+          id: e["id"],
+          validity: e["validity"],
+          validityStr: e["validityStr"],
+          from,
+          to,
+          raw: e["raw"],
+          body: e["body"],
+          parsed: n,
+        });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      alert("Windy API error");
+    });
+}
+onMounted(() => {
+  new ClipboardJS(".btn.copy-metar");
+});
+</script>
--- src/route/index.js
+++ src/route/index.js
@@ -1,16 +1,23 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '../components/Home.vue';
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "../components/Home.vue";
+import Airport from "../components/Airport.vue";
 const routes = [
-    {
-        path: '/:id',
-        name: 'home',
-        component: Home,
-        props: true
-    }
+  {
+    path: "/:id",
+    name: "home",
+    component: Airport,
+    props: true,
+  },
+  {
+    path: "/",
+    name: "index",
+    component: Home,
+    props: true,
+  },
 ];
 
 export default createRouter({
-    history: createWebHashHistory(),
-    routes,
-    linkActiveClass: "active"
+  history: createWebHashHistory(),
+  routes,
+  linkActiveClass: "active",
 });
